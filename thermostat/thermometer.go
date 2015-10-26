16caa648a1fe81ae705e35cd2b33c63788f10434
Tom Taylor <tom@tomtaylor.co.uk>
2015-10-26 07:20:41 +0000

    Trap potential index out of bounds error for Thermometer.

--- thermostat/thermometer.go
+++ thermostat/thermometer.go
@@ -36,13 +36,16 @@ func (t *Thermometer) Temperature() float64 {
 		lines = append(lines, scanner.Text())
 	}
 
-	if lines[0][len(lines[0])-3:] != "YES" {
+	statusLine := lines[0]
+	statusLineLen := len(statusLine)
+
+	if statusLineLen < 3 || statusLine[statusLineLen-3:] != "YES" {
 		log.Fatal("Temperature doesn't seem to be valid")
 	}
 
-	line := lines[1]
-	l := strings.LastIndexAny(line, "t=")
-	celsius, err := strconv.ParseFloat(line[l+1:], 64)
+	dataLine := lines[1]
+	l := strings.LastIndexAny(dataLine, "t=")
+	celsius, err := strconv.ParseFloat(dataLine[l+1:], 64)
 	if err != nil {
 		log.Fatal("Could not convert temperature from device")
 	}
